a6b5f79062062a57d6a2665d5274065f83bdb13c
Soares1xz <93659444+pedrosobrozacom@users.noreply.github.com>
2023-03-10 08:51:25 -0300

    Update views.py

--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1> Olha, se você não me ama</h1>
+                        <h2>caneta azul, azul caneta</h2> ")
